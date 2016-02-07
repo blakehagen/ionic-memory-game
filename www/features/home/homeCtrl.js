83baa54bd51c74751cc299bc6b8f7b236468d8bd
Blake Hagen <blakehagen@gmail.com>
2016-02-06 18:07:52 -0700

    getting random images by random int generator and shuffling order

--- www/features/home/homeCtrl.js
+++ www/features/home/homeCtrl.js
@@ -1,17 +1,40 @@
-angular.module('memoryGame').controller('homeCtrl', function($scope, mainService){
+angular.module('memoryGame').controller('homeCtrl', function ($scope, mainService) {
     
     // $scope.test = 'hello world --> home view'
     
-    $scope.getPonies = function(){
-        mainService.getPonies().then(function(response){
+    $scope.getPonies = function () {
+        var picIds = [];
+        for (var i = 1; i < 11; i++) {
+            var random = Math.floor((Math.random() * 161) + 1);
+            picIds.push(random);
+            picIds.push(random);
+        }
+        console.log(picIds);
+
+        shuffleArray(picIds);
+        console.log(picIds.join(','));
+        
+
+
+        mainService.getPonies(picIds).then(function (response) {
             console.log(response);
             $scope.ponies = response.faces;
         })
     };
-    
+
     $scope.getPonies();
-    
-    
-    
-    
+
+
+    // Randomize array element order in-place.
+    // Using Durstenfeld shuffle algorithm.
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
+
 });
--- www/services/mainService.js
+++ www/services/mainService.js
@@ -1,10 +1,10 @@
 angular.module('memoryGame').service('mainService', function($http, $q){
     
-    this.getPonies = function(){
+    this.getPonies = function(picIds){
         var deferred = $q.defer();
         $http({
             method: 'GET',
-            url: 'http://ponyfac.es/api.json/id:1,2,3,4,5,6,7,8,9,10,22,21,13,14,15'
+            url: 'http://ponyfac.es/api.json/id:' + picIds
         }).then(function(response){
             deferred.resolve(response.data)
         })
